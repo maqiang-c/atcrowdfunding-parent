68e02463487c8d6274b534cbfbfd6184b998e788
ma <1002578306@qq.com>
2020-10-24 22:45:53 +0800

    dev提交hollo

--- spring-security-helloworld/src/main/java/com/atguigu/security/Hello.java
+++ spring-security-helloworld/src/main/java/com/atguigu/security/Hello.java
@@ -3,5 +3,6 @@ package com.atguigu.security;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("nihao ");
+        System.out.println("nihao ");
     }
 }
